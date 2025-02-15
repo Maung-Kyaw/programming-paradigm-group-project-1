6f9fbae1c8fe710d4b35b17dde80af598f41c790
Trinnaya <142000548+Trinnaya@users.noreply.github.com>
2025-02-15 17:52:32 +0700

    Update Project1_6581147.java

--- Project1_6581147.java
+++ Project1_6581147.java
@@ -3,6 +3,12 @@ package Project1_6581147;
 
 import java.util.*;
 
+class InvalidInputException extends Exception{
+    public InvalidInputException(String message){
+        super(message);
+    }
+}
+
 public class Project1_6581147 {
     public static void main(String[] args) {
         HashMap<String,Product> productMap= Product.readProduct();
@@ -10,23 +16,30 @@ public class Project1_6581147 {
             product.displayProduct();
         }
         
-        HashMap<Integer, Installment> installmentMap = Installment.readInstallments();
+        HashMap<Integer, Installment> installmentMap= Installment.readInstallments();
         for(Installment installment : installmentMap.values()){
             installment.displayInstallment();
         }
+        System.out.println();
         
-        HashMap<Integer, Order> orderMap = Order.readOrder();
+        HashMap<Integer, Order> orderMap= Order.readOrder();
         
         for (Order order : orderMap.values()) {
             order.displayOrder(productMap);
         }
         System.out.println();
-
-        Order.orderProcessing(productMap, orderMap, installmentMap);
         
+        System.out.println("=== Order Processing ===");
+        HashMap<String, Customer> customerMap = new HashMap<>();
+        Order.orderProcessing(productMap, orderMap, installmentMap,customerMap);
+        System.out.println();
+        
+        System.out.println("=== Product Summary ===");
         for (Product product : productMap.values()) {
             product.getSalesSummary(orderMap);   
             
         }
+        
+        Order.customerSummary(customerMap);
     }
 }
